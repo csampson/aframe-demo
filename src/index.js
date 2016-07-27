ed22d30cb2bb30318752229345668a622671d24f
Cameron Sampson <csampson.js@gmail.com>
2016-07-27 10:29:56 -0500

    Added several astro bodies

--- src/index.js
+++ src/index.js
@@ -5,22 +5,49 @@ const d3 = require('d3');
 require('aframe');
 require('./index.css');
 
-const planets = [
+const SCALE_FACTOR = 20;
+
+const bodies = [
+  // {
+  //   name: 'Sun',
+  //   texture: '#texture-sun',
+  //   diameter: 1392e3 / SCALE_FACTOR,
+  //   distance: 0
+  // },
   {
-    label: 'Mercury',
-    texture: '#texture-mercury'
+    name: 'Mercury',
+    texture: '#texture-mercury',
+    diameter: 4879 / 1e4,
+    distance: 0.39 * SCALE_FACTOR
   },
   {
-    label: 'Venus',
-    texture: '#texture-mercury'
+    name: 'Venus',
+    texture: '#texture-venus',
+    diameter: 12104 / 1e4,
+    distance: 0.723 * SCALE_FACTOR
+  },
+  {
+    name: 'Earth',
+    texture: '#texture-earth',
+    diameter: 12756 / 1e4,
+    distance: 1 * SCALE_FACTOR
+  },
+  {
+    name: 'Mars',
+    texture: '#texture-mars',
+    diameter: 6794 / 1e4,
+    distance: 1.524 * SCALE_FACTOR
   }
 ];
 
 document.addEventListener('DOMContentLoaded', event => {
   const spheres = d3.select('.space').selectAll('a-sphere');
 
-  spheres.data(planets)
+  spheres.data(bodies)
     .enter()
     .append('a-sphere')
-    .attr('material', data => `src: ${data.texture}`);
+    .attr('id', body => body.name)
+    .attr('material', body => `src: ${body.texture}`)
+    .attr('position', body => `${body.distance} 0 -1`)
+    .attr('scale', body => `${body.diameter} ${body.diameter} ${body.diameter}`);
 });
